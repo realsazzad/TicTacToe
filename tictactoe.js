1acb3ee456b071138eefa3f192eb3ee877067ef6
MD Sazzad Hossain <realsazzad@gmail.com>
2018-02-19 22:35:48 +0600

    Done!

--- tictactoe.js
+++ tictactoe.js
@@ -42,42 +42,90 @@ function play(event)
 
 function check(cell_id,count)
 {
-   if(cell_id=='0' || cell_id=='1' || cell_id=='2')
-       {
-           var x0=document.getElementById("0").innerText;
-           var x1=document.getElementById("1").innerText;
-           var x2=document.getElementById("2").innerText;
-           
-           if(x0==x1 && x1==x2)
-               {
+  
+    var x0=document.getElementById("0").innerText;
+    var x1=document.getElementById("1").innerText;
+    var x2=document.getElementById("2").innerText;
+    var x3=document.getElementById("3").innerText;
+    var x4=document.getElementById("4").innerText;
+    var x5=document.getElementById("5").innerText;
+    var x6=document.getElementById("6").innerText;
+    var x7=document.getElementById("7").innerText;
+    var x8=document.getElementById("8").innerText;
+    
+    if(cell_id=='0')
+        {
+            if((x0==x1 && x1==x2) || (x0==x3 && x3==x6) || (x0==x4 && x4==x8))
+                {
+                   
                    if (x0=="X") winner=1;
                    else winner=2;
-               }
-       }
-    else if(cell_id=='3' || cell_id=='4' || cell_id=='5')
-       {
-           var x0=document.getElementById("3").innerText;
-           var x1=document.getElementById("4").innerText;
-           var x2=document.getElementById("5").innerText;
-           
-           if(x0==x1 && x1==x2) 
-               {
-                   if (x0=="X") winner=1;
+                }
+        }
+    else if(cell_id=='1')
+        {
+            if((x0==x1 && x1==x2) || (x1==x4 && x4==x7) )
+                {
+                   if (x1=="X") winner=1;
                    else winner=2;
-               }
-       }
-    else if(cell_id=='6' || cell_id=='7' || cell_id=='8')
-       {
-           var x0=document.getElementById("6").innerText;
-           var x1=document.getElementById("7").innerText;
-           var x2=document.getElementById("8").innerText;
-           
-           if(x0==x1 && x1==x2) 
-               {
-                   if (x0=="X") winner=1;
+                }
+        }
+     else if(cell_id=='2')
+        {
+            if((x0==x1 && x1==x2) || (x2==x5 && x5==x8) || (x2==x4 && x4==x6))
+                {
+                   if (x2=="X") winner=1;
                    else winner=2;
-               }
-       }
+                }
+        }
+     else if(cell_id=='3')
+        {
+            if((x0==x3 && x3==x6) || (x3==x4 && x4==x5) )
+                {
+                   if (x3=="X") winner=1;
+                   else winner=2;
+                }
+        }
+     else if(cell_id=='4')
+        {
+            if((x3==x4 && x4==x5) || (x1==x4 && x4==x7) || (x0==x4 && x4==x8) || (x2==x4 && x4==x6))
+                {
+                   if (x4=="X") winner=1;
+                   else winner=2;
+                }
+        }
+     else if(cell_id=='5')
+        {
+            if((x2==x5 && x5==x8) || (x3==x4 && x4==x5))
+                {
+                   if (x5=="X") winner=1;
+                   else winner=2;
+                }
+        }
+     else if(cell_id=='6')
+        {
+            if((x0==x3 && x3==x6) || (x6==x7 && x7==x8) || (x2==x4 && x4==x6))
+                {
+                   if (x6=="X") winner=1;
+                   else winner=2;
+                }
+        }
+     else if(cell_id=='7')
+        {
+            if((x1==x4 && x4==x7) || (x6==x4 && x4==x8))
+                {
+                   if (x7=="X") winner=1;
+                   else winner=2;
+                }
+        }
+     else if(cell_id=='8')
+        {
+            if((x2==x5 && x5==x8) || (x6==x7 && x7==x8) || (x0==x4 && x4==x8))
+                {
+                   if (x8=="X") winner=1;
+                   else winner=2;
+                }
+        }
     
 }
 
